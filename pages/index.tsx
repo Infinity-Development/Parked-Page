6208bfbb513f7cb131fa5bd122fbbde08f6d89e4
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-12-12 03:01:24 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,14 +30,6 @@ export default function Home() {
             @InfinityBotList
           </Twitter>
         </FooterText>
-        <svg viewBox="0 0 120 28">
-         <defs> 
-             <path id="wave" d="M 0,10 C 30,10 30,15 60,15 90,15 90,10 120,10 150,10 150,15 180,15 210,15 210,10 240,10 v 28 h -240 z" />
-          </defs> 
-           <use id="wave3" className="wave" xmlnsXlink="#wave" x="0" y="-2" ></use> 
-           <use id="wave2" className="wave" xmlnsXlink="#wave" x="0" y="0" ></use>
-          <use id="wave1" className="wave" xmlnsXlink="#wave" x="0" y="1" />
-        </svg>
       </Footer>
     </Container>
   );
@@ -70,7 +62,6 @@ const Footer = styled.div`
 `;
 
 const FooterInner = styled.div`
-    width: 90%;
     margin-left: auto;
     margin-right: auto;
     margin-bottom: 120px;
